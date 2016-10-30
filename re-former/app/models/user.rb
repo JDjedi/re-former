859babc8bd1fcca97153bd79be184364f7fa78e2
JDjedi <JDjedi1201@gmail.com>
2016-10-30 16:57:25 -0500

    Completed User model, routes, and controller

---
+++ re-former/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	before_save { self.email = self.email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true
+	validates :password, presence: true, length: { minimum: 6 }
+end
