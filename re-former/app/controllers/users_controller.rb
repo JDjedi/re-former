e61b696d90cb53dff5ee94f901a5920944a36b7f
JDjedi <JDjedi1201@gmail.com>
2016-10-30 19:31:33 -0500

    Latest commit

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 	def new
-		
+		@user = User.new
 	end
 
 	def create
-		
+		@user = User.create( username: params[:username], email: params[:email], password: params[:password]) 
+		@user.save
 	end
 end
